8521db7b810f3b3c3ef3cf7cc66d77957db37db5
saurav <sauravy537@gmail.com>
2024-09-10 16:58:40 +0530

    Add project files.

---
+++ Controllers/TeacherController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SMSWEBAPI.Models;
+using static System.Net.WebRequestMethods;
+
+namespace SMS.Controllers
+{
+    public class TeacherController : Controller
+    {
+        HttpClient client;
+
+        public TeacherController()
+        {
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, SslPolicyErrors) => { return true; };
+            client = new HttpClient(clientHandler);
+        }
+       
+        public IActionResult TeacherPro()
+        {
+            string url =" https://localhost:7264/api/Teacher/GetTeacherById?id=2";
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            Teacher teachers = new Teacher();
+            if (response.IsSuccessStatusCode)
+            {
+                var jsondata = response.Content.ReadAsStringAsync().Result;
+                teachers = JsonConvert.DeserializeObject<Teacher>(jsondata);
+            }
+
+            return View(teachers);
+
+        }
+
+
+        public IActionResult StudentAtt()
+        {
+
+        }
+
+
+
+
+    }
+}
